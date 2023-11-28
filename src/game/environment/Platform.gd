5bba9a879977088c473f5e24a61cc07469f2c289
fabriciomarote <fabrii.cai93@gmail.com>
2023-11-28 01:23:24 -0300

    Se modifica estructura de level, se amplia terreno para tutorial

--- src/game/environment/Platform.gd
+++ src/game/environment/Platform.gd
@@ -2,7 +2,8 @@ extends StaticBody2D
 
 
 func _on_Area2D_body_entered(body):
-	if body.has_method("notify_hit") && !body.protection_actived:
-		body.notify_hit()
-	if body.has_method("notify_hit_protection") && body.protection_actived:
-		body.notify_hit_protection()
+	if body is Player:
+		if body.has_method("notify_hit") && !body.protection_actived:
+			body.notify_hit(1)
+		if body.has_method("notify_hit_protection") && body.protection_actived:
+			body.notify_hit_protection()
